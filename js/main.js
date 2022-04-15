9c837dca6614d4d41d3ddfce898556c4ec7eb1cf
ROIJA <32898061+ROIJA@users.noreply.github.com>
2022-04-15 12:52:36 +0630

    Update main.js

--- js/main.js
+++ js/main.js
@@ -154,9 +154,9 @@ jQuery(document).ready(function(jQuery) {
     jQuery(window).on("scroll", function() {
         // 4. to top arrow animation
         if (jQuery(this).scrollTop() > 500) {
-            jQuery(".to-top-arrow").addClass("show");
+            jQuery(".to-top").addClass("show");
         } else {
-            jQuery(".to-top-arrow").removeClass("show");
+            jQuery(".to-top").removeClass("show");
         }
     });
 	
@@ -167,3 +167,62 @@ jQuery(document).ready(function(jQuery) {
         }, 800);
         return false;
     });
+	
+	
+	
+// food category
+
+let foodMenuList = document.querySelector('.food-item-wrap')
+
+let foodCategory = document.querySelector('.food-category')
+
+let categories = foodCategory.querySelectorAll('button')
+
+Array.from(categories).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currCat = foodCategory.querySelector('button.active')
+        currCat.classList.remove('active')
+        e.target.classList.add('active')
+        foodMenuList.classList ='food-item-wrap '+ e.target.getAttribute('data-food-type')
+    }
+})
+
+// on scroll animation
+
+let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
+
+let elToShow = document.querySelectorAll('.play-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    return (
+        (rect.top <= 0 && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    )
+}
+
+loop = () => {
+    elToShow.forEach((item, index) => {
+        if (isElInViewPort(item)) {
+            item.classList.add('start')
+        } else {
+            item.classList.remove('start')
+        }
+    })
+
+    scroll(loop)
+}
+
+loop()
+
+
+
+
+
+
+
+
