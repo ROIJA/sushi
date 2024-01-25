9a9e581622816faf467f04b3bc0d07cc45a6e01f
ROIJA <32898061+ROIJA@users.noreply.github.com>
2024-01-25 13:25:13 +0630

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ if ($(this).scrollTop() > 100){
   }
 });
 */
+ //---------------------------------------------------------
+// Page Scroll
+//--------------------------------------------------------- 
+
+//scroll to anchor
+
+$('a.page-scroll').click(function(){
+    if ($(this).scrollTop() > 800) {
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top-165
+        }, 800);
+        return false;
+    } else {
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top-165
+        }, 800);
+        return false;
+    }
+});
+
+
 /* ------------------------------------------------------------ */
 /* Mobile Menu - 
 /* ------------------------------------------------------------ */
@@ -315,9 +336,11 @@ window.addEventListener('load', function() {
 	}
 });
 
+
 /* ------------------------------------------------------------ */
-/*  SHOW SCROLL UP
+/*  SHOW SCROLL UP - button
 /* ------------------------------------------------------------ */
+
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
@@ -325,73 +348,36 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp);
 
-
 /* ------------------------------------------------------------ */
-/*  Location Image Slider
+/*  Location Image Slider @home
 /* ------------------------------------------------------------ */
-
-$(document).ready(function() {
+$(document).ready(function () {
 	// Swiper
-	function LocationSlider(){
-		
-		new Swiper(".swiper-container", {
+	new Swiper(".swiper-container", {
 			
-			effect: "coverflow",
-			spaceBetween: 28,
-			zoom: !0,
-			speed: 500,
-			grabCursor: !0,
-			rewind: !0,  
-			centeredSlides: false,
-			coverflowEffect: {
-			  rotate: 0,
-			  stretch: 0,
-			  depth: 0,
-			  modifier: 1,
-			  slideShadows: false
-			},		
-			slidesPerView: "auto",
-			breakpoints: { 768: { spaceBetween: 38 } },
-			scrollbar: { el: ".swiper-scrollbar", draggable: true },
+		effect: "coverflow",
+		spaceBetween: 30,
+		zoom: !0,
+		speed: 500,
+		grabCursor: !0,
+		rewind: !0,  
+		centeredSlides: false,
+		coverflowEffect: {
+		  rotate: 0,
+		  stretch: 0,
+		  depth: 0,
+		  modifier: 1,
+		  slideShadows: false
+		},		
+		slidesPerView: "auto",
+		breakpoints: { 768: { spaceBetween: 38 } },
+		scrollbar: { el: ".swiper-scrollbar", draggable: true },
 			
-		});
-	}
-
-	LocationSlider();
+	});
 });
-/*
-	// Swiper
-	function LocationSlider(){
-		
-		new Swiper(".swiper-container", {
-			
-			effect: "coverflow",
-			spaceBetween: 28,
-			zoom: !0,
-			speed: 500,
-			grabCursor: !0,
-			rewind: !0,  
-			centeredSlides: false,
-			coverflowEffect: {
-			  rotate: 0,
-			  stretch: 0,
-			  depth: 0,
-			  modifier: 1,
-			  slideShadows: false
-			},		
-			slidesPerView: "auto",
-			breakpoints: { 768: { spaceBetween: 38 } },
-			scrollbar: { el: ".swiper-scrollbar", draggable: true },
-			
-		});
-	}
-
-	LocationSlider();
-});
-*/
 
 /* ------------------------------------------------------------ */
-/*  Lightbox
+/*  Lightbox for food gallery
 /* ------------------------------------------------------------ */
 const lightbox = GLightbox({
   touchNavigation: true,
@@ -401,11 +387,9 @@ const lightbox = GLightbox({
 });
 	
 /* ------------------------------------------------------------ */
-/*  locationHero
+/*  locationHero - location MENU pages
 /* ------------------------------------------------------------ */
-
     //=====  Slick Showcase active
-  
     $('.locationHero-active').slick({
         dots: false,
         infinite: true,
@@ -432,112 +416,85 @@ const lightbox = GLightbox({
 
 		
         ]
-    });
- 
- 
+    }); 
+	
 
-/* ------------------------------------------------------------ */
-/*  Page Hero Slider
-/* ------------------------------------------------------------ */
+	
+/*=============== // ===============*/ 
+// Click function for show the Modal
+// Get modal element
+const modal = document.getElementById('AppModal');
+// All page modals
+var modals = document.querySelectorAll('.modal');
+// Get open modal button
+const modalBtn = document.querySelectorAll('.modal-btn');
+// Get close button
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-$('.slider').each(function() {
-  var $this = $(this);
-  var $group = $this.find('.slide_group');
-  var $slides = $this.find('.slide');
-  var bulletArray = [];
-  var currentIndex = 0;
-  var timeout;
-  
-  function move(newIndex) {
-    var animateLeft, slideLeft;
-    
-    advance();
-    
-    if ($group.is(':animated') || currentIndex === newIndex) {
-      return;
-    }
-    
-    bulletArray[currentIndex].removeClass('active');
-    bulletArray[newIndex].addClass('active');
-    
-    if (newIndex > currentIndex) {
-      slideLeft = '100%';
-      animateLeft = '-100%';
-    } else {
-      slideLeft = '-100%';
-      animateLeft = '100%';
-    }
-    
-    $slides.eq(newIndex).css({
-      display: 'block',
-      left: slideLeft
-    });
-    $group.animate({
-      left: animateLeft
-    }, function() {
-      $slides.eq(currentIndex).css({
-        display: 'none'
-      });
-      $slides.eq(newIndex).css({
-        left: 0
-      });
-      $group.css({
-        left: 0
-      });
-      currentIndex = newIndex;
-    });
+const closeBtn2 = document.getElementsByClassName('closeBtn2')[0];
+// Listen 	for OPEN Click
+modalBtn.forEach(function(e) {
+e.addEventListener('click', openModal);
+})
+// Listen for CLOSE Click
+closeBtn.addEventListener('click', closeModal);
+closeBtn2.addEventListener('click', closeModal);
+// Listen for OUTSIDE Click
+window.addEventListener('click', outsideClick);
+
+// Function to OPEN modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Function to CLOSE modal
+function closeModal() {
+  modal.style.display = "none";
+}
+// Function to CLOSE modal
+function outsideClick(e) {
+  if(e.target == modal) {
+    modal.style.display = "none";
   }
-  
-  function advance() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 4000);
-  }
-  
-  $('.next_btn').on('click', function() {
-    if (currentIndex < ($slides.length - 1)) {
-      move(currentIndex + 1);
-    } else {
-      move(0);
-    }
-  });
-  
-  $('.previous_btn').on('click', function() {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
-  });
-  
-  $.each($slides, function(index) {
-    var $button = $('<a class="slide_btn">&bull;</a>');
-    
-    if (index === currentIndex) {
-      $button.addClass('active');
-    }
-    $button.on('click', function() {
-      move(index);
-    }).appendTo('.slide_buttons');
-    bulletArray.push($button);
-  });
-  
-  advance();
+}
+
+//---------------------------------------------------------
+// Click function for show the Modal
+//---------------------------------------------------------
+$('[data-modal]').click(function(e){
+  e.preventDefault();
+  $(this).parent().find('[data-wrapper]').addClass('active');
 });
 
-/*=============== // ===============*/ 
+$('[data-wrapper]').click(
+  function(modal){
+  if (modal.target == this) {
+    $(this).removeClass('.modal-wrapper active');
+  }
+});
+
+$('[data-dismiss]').click(function(e){
+	  e.preventDefault();
+	$('[data-wrapper]').removeClass('active');
+});
+/*
+       $(document).mouseup(function (e) {
+            if ($(e.target).
+                closest(".box-modal").
+                length=== 0) {
+                $(".box-modal").hide();
+            }
+        }); 
+		*/
 
 
-/* ------------------------------------------------------------ */
-/*  Lazy Loading - image
-/* ------------------------------------------------------------ */
+//---------------------------------------------------------
+// Lazy Loading - image
+//---------------------------------------------------------
 
 $("img.lazy, img.lazyload").lazyload({
     effect : "fadeIn"
 });
-      
+
+/*=============== Location HOver IMAGE ===============*/       
+
